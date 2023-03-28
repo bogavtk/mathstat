746efebebd962d6e42c3a2e2da31239bca6c4109
Boga <bogdan.vtk.4516@gmail.com>
2023-03-28 13:18:28 +0300

    r2z1

--- r2/r2z1.py
+++ r2/r2z1.py
@@ -1,6 +1,8 @@
 import csv
+from scipy.stats import t
+from math import sqrt
 
-filename = r"C:\Users\Bogdan\Desktop\mathstat\3\r2z1.csv"
+filename = r"C:\Users\Bogdan\Desktop\mathstat\15\r2z1.csv"
 x_elem = []
 y = []
 with open(filename, "r") as csvfile:
@@ -17,3 +19,31 @@ n2 = len(y)
 
 x_mean = sum(x) / n1
 y_mean = sum(y) / n2
+print(f"Среднее значение 1 выборки: {x_mean}")
+print(f"Среднее значение 2 выборки: {y_mean}")
+
+df = n1 + n2 - 2  # Число степеней свободы
+
+alpha = 0.01
+
+variance_x = sum((x - x_mean) ** 2 for x in x) / n1
+variance_y = sum((y - y_mean) ** 2 for y in y) / n2
+
+critical_const = t.ppf(1 - alpha / 2, df)
+t_stats = ((x_mean - y_mean) / sqrt(n1 * variance_x + n2 * variance_y)) * (sqrt((n1 * n2 * df) / (n1 + n2)))
+abs_t_stats = abs(t_stats)
+p_value = 2 * (1 - t.cdf(abs(t_stats), n1 + n2 - 2))
+
+if abs_t_stats > critical_const:
+    print('Отвергаем H0')
+elif abs_t_stats < critical_const:
+    print('Не отвергаем H0')
+
+if p_value > alpha:
+    print("Гипотеза 0 не можем отвергнуть => различия между выборками не являются статистически значимыми")
+else:
+    print("Гипотеза 0 отвергается => различия между выборками статистически значимы")
+
+print(f"Значение t-статистики: {t_stats}")
+print(f"Критическая константа: {critical_const}")
+print(f"p-value: {p_value}")
--- r2/r2z2.py
+++ r2/r2z2.py
@@ -1,48 +1,23 @@
-import numpy as np
 import csv
 
-filename = r"C:\Users\Bogdan\Desktop\mathstat\3\r2z1.csv"
-x_elem = []
-y = []
-with open(filename, "r") as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader)
-    for row in reader:
-        x_elem.append(row[0])
-        y.append(float(row[1]))
+data = []
 
-x = [float(x) for x in x_elem if x != ""]
+with open(r'C:\Users\Bogdan\Desktop\mathstat\3\r2z2.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    for row in csv_reader:
+        data.append(float(row[0]))
 
-
-def kolmogorov_smirnov_test(x, y, alpha):
-    """
-    Функция для проведения КС-теста без использования библиотек
-    x: выборка 1
-    y: выборка 2
-    alpha: уровень значимости
-    """
-    n1 = len(x)
-    n2 = len(y)
-    n = n1 + n2
-
-    # объединяем две выборки и сортируем их
-    merged_data = np.sort(np.concatenate([x, y]))
-
-    # рассчитываем эмпирическую функцию распределения (ECDF) для каждой выборки
-    ecdf_x = np.searchsorted(x, merged_data, side='right') / n1
-    ecdf_y = np.searchsorted(y, merged_data, side='right') / n2
-
-    # рассчитываем значение статистики КС
-    d = np.max(np.absolute(ecdf_x - ecdf_y))
-
-    # рассчитываем критическое значение статистики КС
-    c_alpha = np.sqrt(-0.5 * np.log(alpha / 2)) / np.sqrt(n)
-
-    # сравниваем значение статистики КС с критическим значением и принимаем решение
-    if d > c_alpha:
-        return False
-    else:
-        return True
-
-
-print(kolmogorov_smirnov_test(x, y, 0.01))
+# x = np.linspace(min(data), max(data), 100)
+# cdf = stats.t.cdf(x, len(data) - 1)
+# plt.plot(x, cdf, label='Функция распределения')
+#
+# # Строим график эмпирической функции распределения (ЭФР)
+# x_sorted = np.sort(data)
+# y = np.arange(1, len(data) + 1) / len(data)
+#
+# # Строим график ЭФР
+# plt.step(x_sorted, y, label='ЭФР')
+#
+# plt.legend()
+# plt.show()

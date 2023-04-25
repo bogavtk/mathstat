d0d7484d617c95de03bc14296239ea6c9a2626fb
Boga <bogdan.vtk.4516@gmail.com>
2023-04-25 19:09:12 +0300

    Linar r3

--- r3/Linar/r3z1.py
+++ r3/Linar/r3z1.py
@@ -1,8 +1,9 @@
 import csv
+from math import sqrt
 
 data = []
 
-with open('/home/bogdan/Рабочий стол/study/mathstat/15/r3z1.csv') as csv_file:
+with open(r'C:\Users\Bogdan\Desktop\mathstat\15\r3z1.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
     for row in csv_reader:
@@ -11,6 +12,10 @@ with open('/home/bogdan/Рабочий стол/study/mathstat/15/r3z1.csv') as 
 data_square = [x ** 2 for x in data]
 
 n = len(data)
-mean = sum(data) / n
 
+mean = sum(data) / n
 mean_square = sum(data_square) / n
+
+sigma = sqrt(mean_square / n)
+
+print(f"Найденое значение оценки параметра σ: {sigma}")
